82c80530fe02a56088355cbce205543c45acb053
gvanbo <gvanbo@gmail.com>
2021-04-29 09:47:54 -0600

    Add project files.

---
+++ Demo MBA Books/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace Demo_MBA_Books
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustID = new System.Windows.Forms.Label();
+            this.txtCustID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblZIP = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZIP = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDiscard = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.customerIdCBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblCustID
+            // 
+            this.lblCustID.AutoSize = true;
+            this.lblCustID.Location = new System.Drawing.Point(63, 57);
+            this.lblCustID.Name = "lblCustID";
+            this.lblCustID.Size = new System.Drawing.Size(73, 15);
+            this.lblCustID.TabIndex = 0;
+            this.lblCustID.Text = "Customer ID";
+            // 
+            // txtCustID
+            // 
+            this.txtCustID.Location = new System.Drawing.Point(539, 54);
+            this.txtCustID.Name = "txtCustID";
+            this.txtCustID.ReadOnly = true;
+            this.txtCustID.Size = new System.Drawing.Size(36, 23);
+            this.txtCustID.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(155, 99);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(342, 23);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(63, 107);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 15);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(63, 154);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(49, 15);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(155, 146);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(342, 23);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(63, 199);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(28, 15);
+            this.lblCity.TabIndex = 6;
+            this.lblCity.Text = "City";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(63, 239);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(33, 15);
+            this.lblState.TabIndex = 7;
+            this.lblState.Text = "State";
+            // 
+            // lblZIP
+            // 
+            this.lblZIP.AutoSize = true;
+            this.lblZIP.Location = new System.Drawing.Point(63, 271);
+            this.lblZIP.Name = "lblZIP";
+            this.lblZIP.Size = new System.Drawing.Size(24, 15);
+            this.lblZIP.TabIndex = 8;
+            this.lblZIP.Text = "ZIP";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(155, 191);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(342, 23);
+            this.txtCity.TabIndex = 9;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(539, 231);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(51, 23);
+            this.txtState.TabIndex = 10;
+            // 
+            // txtZIP
+            // 
+            this.txtZIP.Location = new System.Drawing.Point(155, 271);
+            this.txtZIP.Name = "txtZIP";
+            this.txtZIP.Size = new System.Drawing.Size(342, 23);
+            this.txtZIP.TabIndex = 11;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Enabled = false;
+            this.btnBack.Location = new System.Drawing.Point(301, 53);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(49, 23);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(357, 54);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(49, 23);
+            this.btnForward.TabIndex = 13;
+            this.btnForward.Text = ">>";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(166, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDiscard
+            // 
+            this.btnDiscard.Location = new System.Drawing.Point(275, 323);
+            this.btnDiscard.Name = "btnDiscard";
+            this.btnDiscard.Size = new System.Drawing.Size(75, 23);
+            this.btnDiscard.TabIndex = 15;
+            this.btnDiscard.Text = "&Discard";
+            this.btnDiscard.UseVisualStyleBackColor = true;
+            this.btnDiscard.Click += new System.EventHandler(this.btnDiscard_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(416, 323);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 16;
+            this.addBtn.Text = "&Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Location = new System.Drawing.Point(213, 231);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(121, 23);
+            this.stateComboBox.TabIndex = 17;
+            this.stateComboBox.SelectedIndexChanged += new System.EventHandler(this.stateComboBox_SelectedIndexChanged);
+            // 
+            // customerIdCBox
+            // 
+            this.customerIdCBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.customerIdCBox.FormattingEnabled = true;
+            this.customerIdCBox.Location = new System.Drawing.Point(155, 53);
+            this.customerIdCBox.Name = "customerIdCBox";
+            this.customerIdCBox.Size = new System.Drawing.Size(121, 23);
+            this.customerIdCBox.TabIndex = 18;
+            this.customerIdCBox.SelectedIndexChanged += new System.EventHandler(this.customerIdCBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customerIdCBox);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.btnDiscard);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.txtZIP);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblZIP);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtCustID);
+            this.Controls.Add(this.lblCustID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustID;
+        private System.Windows.Forms.TextBox txtCustID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblZIP;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZIP;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDiscard;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.ComboBox customerIdCBox;
+    }
+}
+
---
+++ Demo MBA Books/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Demo_MBA_Books.Models;
+
+namespace Demo_MBA_Books
+{
+    public partial class Form1 : Form
+    {
+        MMABooksContext context;
+        Customer currentCustomer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            context = new MMABooksContext();
+            currentCustomer = context.Customers.First(); //Load the first Customer
+        //Loading all states data in the combo-box
+            stateComboBox.DataSource = context.States.ToList();
+            stateComboBox.DisplayMember = "StateName";
+            stateComboBox.ValueMember = "StateCode";
+            //Load all customer IDs
+            Object[] cIds = context.Customers.Select(c => (Object)c.CustomerId).ToArray();
+            customerIdCBox.Items.AddRange(cIds);
+            DisplayCustomer();
+
+        }
+
+        /// <summary>
+        /// Display the current customer in the Form fields
+        /// </summary>
+        private void DisplayCustomer()
+        {
+            if (currentCustomer is null) return;
+            //txtCustID.Text = currentCustomer.CustomerId.ToString();
+            customerIdCBox.SelectedItem = currentCustomer.CustomerId;
+            txtName.Text = currentCustomer.Name;
+            txtAddress.Text = currentCustomer.Address;
+            txtCity.Text = currentCustomer.City;
+            txtState.Text = currentCustomer.State;
+            txtZIP.Text = currentCustomer.ZipCode;
+            stateComboBox.SelectedValue = currentCustomer.State;
+
+            if (customerIdCBox.SelectedIndex > 0) btnBack.Enabled = true;
+            else btnBack.Enabled = false;
+
+            if (customerIdCBox.SelectedIndex == customerIdCBox.Items.Count -1) btnForward.Enabled = false;
+            else btnForward.Enabled = true;
+
+
+            //if (customerIdCBox.Items.Count <= index)
+            //{
+            //    btnForward.Enabled = false;
+            //    return;
+            //}
+
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            int index = customerIdCBox.SelectedIndex +1; // Get the next Index of the current customer ID
+            int id = Int32.Parse(customerIdCBox.Items[index].ToString()); //currentCustomer.CustomerId + 1;
+            currentCustomer = context.Customers.Find(id);
+            DisplayCustomer();
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+            int index = customerIdCBox.SelectedIndex - 1; // Get the previous Index of the current customer ID
+            int id = Int32.Parse(customerIdCBox.Items[index].ToString()); //currentCustomer.CustomerId + -1;
+            currentCustomer = context.Customers.Find(id);
+            DisplayCustomer();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            currentCustomer.Name = txtName.Text;
+            currentCustomer.Address = txtAddress.Text;
+            currentCustomer.City = txtCity.Text;
+            //currentCustomer.State = txtState.Text;
+            currentCustomer.ZipCode = txtZIP.Text;
+            context.SaveChanges();
+            DisplayCustomer();
+        }
+
+        private void btnDiscard_Click(object sender, EventArgs e)
+        {
+            context.Customers.Remove(currentCustomer);
+            context.SaveChanges();
+            currentCustomer = context.Customers.First();
+            DisplayCustomer();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            Customer addOne = new Customer()
+            {
+                Name = txtName.Text,
+                Address = txtAddress.Text,
+                City = txtCity.Text,
+                //State = txtState.Text,
+                ZipCode = txtZIP.Text,
+                State = stateComboBox.SelectedValue.ToString(),
+            };
+
+            context.Customers.Add(addOne);
+            context.SaveChanges();
+            currentCustomer = addOne;
+            DisplayCustomer();
+        }
+
+        private void stateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void customerIdCBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(customerIdCBox.SelectedItem);
+            currentCustomer = context.Customers.Find(id);
+            DisplayCustomer();
+        }
+    }
+}
